c14ed5f77cc61a8204f3590c7f36e00e3e1b5d10
Nikos Papadopulos <nikozzzzzz@gmail.com>
2018-08-15 13:18:11 +0500

    добавил еще поисковиков
    добавил поисковики и комменты, засунул все в три колонки бутстрапа

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-- //бутстрап чтобы томат не охуел верстать это все -->
+
 <!-- Latest compiled and minified CSS -->
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -5,20 +7,20 @@
     <!-- Latest compiled and minified JavaScript -->
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<?php
     
+<!-- //если есть пост, то делает всю работу    --> 
+<?php    
 if(isset($_POST['submit'])) {
-    echo "welcome to shitScriptStudios";
-    echo "<br>";
+    
       
  $query = $_POST['query'];
-    echo $query;
+   // echo $query;
     $queryNoSpace =  str_replace (" ", "+", $query);
-	echo $queryNoSpace;
-}
-  
-    
+           
+} 
     ?>
+
+<!-- //хтмл для красного  -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,27 +28,45 @@ if(isset($_POST['submit'])) {
     <title>Document</title>
 </head>
 <body>
+    
 <div class="container">
   <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      <form action="index.php" method="post">
-    <input type="text" name="query" placeholder="input query">
+      
+    <div class="col-md-4">
+   <?php 	echo "Ваш запрос: ".$queryNoSpace;
+    echo "<br>welcome to shitScriptStudios";
+    echo "<br>"; 
+    ?>
+    </div> 
+    <div class="col-md-4">
+    <!--  //вот основная форма ввода данных и отправки, не ломай -->
+        <form action="index.php" method="post">
+    <input type="text" name="query" placeholder="введите запчасть">
     <input type="submit" name="submit">
     
-</form> 
-<br>
-<br>
-<br>
-<a href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo $query; ?>&dispatch=products.search">Ссылка на сириус</a>               
+        </form> 
+     <!--       //форма кончилась, можешь дальше ломать все нахуй  -->
 
-     
     </div>
-    <div class="col-sm">
-      One of three columns
+    <div class="col-md-4">
+        
+        
+
+ <!--       // вот пошли ссылки, тут можешь верстать  -->
+        <a target="_blank" href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo $query; ?>&dispatch=products.search">Ссылка на сириус</a>               
+        <br>
+        <a target="_blank" href="https://detalka.ru/index.php?route=product/search&search=<?php echo $query; ?>&description=true&sub_category=true">Ссылка на детальку</a>   
+        <br>
+        <a target="_blank" href="http://axmobi.ru/kategoryja/all?search=<?php echo $query; ?>">Ссылка на аксмоби</a>
+        <br>
+        <a target="_blank" href="http://green-spark.ru/search/?q=<?php echo $query; ?>&section=0&only_stock=no">Ссылка на гринспарк</a>
+        <br>
+        <a target="_blank" href="https://ekaterinburg.axeum.ru/category/all?search=<?php echo $query; ?>&search_area=">Ссылка на аксеум</a>
+ <!--        //ссылки кончились нахуй  -->
+  
+        
     </div>
+   
   </div>
 </div>
     
