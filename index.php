6c07b317bb01ae80a63712adab7d3292f4f146fb
Nikos P. <nikozzzzzz@gmail.com>
2018-08-15 20:01:56 +0500

    переименовал bestFunc по-нормальному, поправил комментарии к коду

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- //бутстрап чтобы томат не охуел верстать это все -->
+<!-- бутстрап чтобы томат не охуел верстать это все -->
 
 <!-- Latest compiled and minified CSS -->
 
@@ -8,9 +8,9 @@
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
-<!-- //если есть пост, то делает всю работу    --> 
+<!-- если есть пост, то делает всю работу    --> 
 <?php 
-function bestFunc() {
+function getPostAndReplaceSpace() {
 $query = $_POST['query'];
    // echo $query;
    // echo "<br>";
@@ -21,11 +21,11 @@ $query = $_POST['query'];
 if(isset($_POST['submit'])) {
     
       
- bestFunc();
+ getPostAndReplaceSpace();
 } 
     ?>
 
-<!-- //хтмл для красного  -->
+<!-- хтмл для красного  -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,36 +38,36 @@ if(isset($_POST['submit'])) {
   <div class="row">
       
     <div class="col-md-4">
-   <?php 	echo "Ваш запрос: ". bestFunc();
+   <?php 	echo "Ваш запрос: ". getPostAndReplaceSpace();
     echo "<br>welcome to shitScriptStudios";
     echo "<br>"; 
     ?>
     </div> 
     <div class="col-md-4">
-    <!--  //вот основная форма ввода данных и отправки, не ломай -->
+    <!--  вот основная форма ввода данных и отправки, не ломай -->
         <form action="index.php" method="post">
     <input type="text" name="query" placeholder="введите запчасть">
     <input type="submit" name="submit">
     
         </form> 
-     <!--       //форма кончилась, можешь дальше ломать все нахуй  -->
+     <!-- форма кончилась, можешь дальше ломать все нахуй  -->
 
     </div>
     <div class="col-md-4">
         
         
 
- <!--       // вот пошли ссылки, тут можешь верстать  -->
-        <a target="_blank" href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo bestFunc(); ?>&dispatch=products.search">Ссылка на сириус</a>               
+ <!--        вот пошли ссылки, тут можешь верстать  -->
+        <a target="_blank" href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo getPostAndReplaceSpace(); ?>&dispatch=products.search">Ссылка на сириус</a>               
         <br>
-        <a target="_blank" href="https://detalka.ru/index.php?route=product/search&search=<?php echo bestFunc(); ?>&description=true&sub_category=true">Ссылка на детальку</a>   
+        <a target="_blank" href="https://detalka.ru/index.php?route=product/search&search=<?php echo getPostAndReplaceSpace(); ?>&description=true&sub_category=true">Ссылка на детальку</a>   
         <br>
-        <a target="_blank" href="http://axmobi.ru/kategoryja/all?search=<?php echo bestFunc(); ?>">Ссылка на аксмоби</a>
+        <a target="_blank" href="http://axmobi.ru/kategoryja/all?search=<?php echo getPostAndReplaceSpace(); ?>">Ссылка на аксмоби</a>
         <br>
-        <a target="_blank" href="http://green-spark.ru/search/?q=<?php echo bestFunc(); ?>&section=0&only_stock=no">Ссылка на гринспарк</a>
+        <a target="_blank" href="http://green-spark.ru/search/?q=<?php echo getPostAndReplaceSpace(); ?>&section=0&only_stock=no">Ссылка на гринспарк</a>
         <br>
-        <a target="_blank" href="https://ekaterinburg.axeum.ru/category/all?search=<?php echo bestFunc(); ?>&search_area=">Ссылка на аксеум</a>
- <!--        //ссылки кончились нахуй  -->
+        <a target="_blank" href="https://ekaterinburg.axeum.ru/category/all?search=<?php echo getPostAndReplaceSpace(); ?>&search_area=">Ссылка на аксеум</a>
+ <!-- ссылки кончились нахуй  -->
   
         
     </div>
