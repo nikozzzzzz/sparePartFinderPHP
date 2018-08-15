29849207dbc5572a4c69b3f856cd7258b0ca66ff
Nikos Papadopulos <nikozzzzzz@gmail.com>
2018-08-15 19:56:45 +0500

    перенес часть в функцию
    облагородил, заменил везде $query на саму функцию

--- index.php
+++ index.php
@@ -9,14 +9,19 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
 <!-- //если есть пост, то делает всю работу    --> 
+<?php 
+function bestFunc() {
+$query = $_POST['query'];
+   // echo $query;
+   // echo "<br>";
+    return $queryNoSpace =  str_replace (" ", "+", $query);  
+}
+?>
 <?php    
 if(isset($_POST['submit'])) {
     
       
- $query = $_POST['query'];
-   // echo $query;
-    $queryNoSpace =  str_replace (" ", "+", $query);
-           
+ bestFunc();
 } 
     ?>
 
@@ -33,7 +38,7 @@ if(isset($_POST['submit'])) {
   <div class="row">
       
     <div class="col-md-4">
-   <?php 	echo "Ваш запрос: ".$queryNoSpace;
+   <?php 	echo "Ваш запрос: ". bestFunc();
     echo "<br>welcome to shitScriptStudios";
     echo "<br>"; 
     ?>
@@ -53,15 +58,15 @@ if(isset($_POST['submit'])) {
         
 
  <!--       // вот пошли ссылки, тут можешь верстать  -->
-        <a target="_blank" href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo $query; ?>&dispatch=products.search">Ссылка на сириус</a>               
+        <a target="_blank" href="http://siriust.ru/?subcats=Y&pcode_from_q=Y&pshort=Y&pfull=Y&pname=Y&pkeywords=Y&search_performed=Y&pshort=Y&pfull=N&pname=Y&pkeywords=N&pcode=Y&match=all&q=<?php echo bestFunc(); ?>&dispatch=products.search">Ссылка на сириус</a>               
         <br>
-        <a target="_blank" href="https://detalka.ru/index.php?route=product/search&search=<?php echo $query; ?>&description=true&sub_category=true">Ссылка на детальку</a>   
+        <a target="_blank" href="https://detalka.ru/index.php?route=product/search&search=<?php echo bestFunc(); ?>&description=true&sub_category=true">Ссылка на детальку</a>   
         <br>
-        <a target="_blank" href="http://axmobi.ru/kategoryja/all?search=<?php echo $query; ?>">Ссылка на аксмоби</a>
+        <a target="_blank" href="http://axmobi.ru/kategoryja/all?search=<?php echo bestFunc(); ?>">Ссылка на аксмоби</a>
         <br>
-        <a target="_blank" href="http://green-spark.ru/search/?q=<?php echo $query; ?>&section=0&only_stock=no">Ссылка на гринспарк</a>
+        <a target="_blank" href="http://green-spark.ru/search/?q=<?php echo bestFunc(); ?>&section=0&only_stock=no">Ссылка на гринспарк</a>
         <br>
-        <a target="_blank" href="https://ekaterinburg.axeum.ru/category/all?search=<?php echo $query; ?>&search_area=">Ссылка на аксеум</a>
+        <a target="_blank" href="https://ekaterinburg.axeum.ru/category/all?search=<?php echo bestFunc(); ?>&search_area=">Ссылка на аксеум</a>
  <!--        //ссылки кончились нахуй  -->
   
         
