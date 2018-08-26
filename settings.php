df0bd38c1ee0017eb9f8c5e60f190afd8dcb99b3
Nikos P <nikozzzzzz@gmail.com>
2018-08-26 17:05:25 +0500

    add mysql settings file

--- settings.php
+++ settings.php
@@ -6,3 +6,16 @@
  * and open the template in the editor.
  */
 
+$servername = "localhost";
+$username = "spareFinder";
+$password = "Gotohell5122";
+$database = "spareLogin";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
