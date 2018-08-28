f23d26054ea21bbbc32514bf6d9957988fbe5057
Nikos P <nikozzzzzz@gmail.com>
2018-08-28 11:47:06 +0500

    s

--- login.php
+++ login.php
@@ -9,6 +9,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
  <?php 
+ //define login ahead to fix notice
+$login = "";
 $servername = "localhost";
 $dbusername = "spareFinder";
 $dbpassword = "Gotohell5122";
